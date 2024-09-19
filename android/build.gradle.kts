339b4988e19e4ae6ed521348e4e77fdbea3cb0bd
Andrea Quatrale <andrea.quatrale@musixmatch.com>
2024-09-19 18:03:17 +0200

    Moved build.gradle back to groovy script

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,6 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        //noinspection AndroidGradlePluginVersion
         classpath("com.android.tools.build:gradle:4.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
     }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,15 +1,2 @@
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        mavenLocal()
-        maven {
-            // All of React Native (JS, Obj-C sources, Android binaries) is installed from npm
-            url = uri("$rootDir/../node_modules/react-native/android")
-        }
-    }
-}
-
 rootProject.name = "Spotify Remote SDK"
 include(":spotify-remote-sdk")
